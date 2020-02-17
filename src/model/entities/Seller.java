3910da6d1ced067136069e492fe05f5c2ab30ba7
nathanjsoares <nathansoares@gmail.com>
2020-02-17 20:15:51 -0300

    seller entity class

---
+++ src/model/entities/Seller.java
@@ -0,0 +1,106 @@
+package model.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Seller implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private int id;
+	private String name;
+	private String email;
+	private Date birthDate;
+	private double baseSalary;
+	
+	private Department departmant;
+	
+	public Seller() {
+		
+	}
+
+	public Seller(int id, String name, String email, Date birthDate, double baseSalary, Department departmant) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.birthDate = birthDate;
+		this.baseSalary = baseSalary;
+		this.departmant = departmant;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public double getBaseSalary() {
+		return baseSalary;
+	}
+
+	public void setBaseSalary(double baseSalary) {
+		this.baseSalary = baseSalary;
+	}
+
+	public Department getDepartmant() {
+		return departmant;
+	}
+
+	public void setDepartmant(Department departmant) {
+		this.departmant = departmant;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Seller other = (Seller) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Seller [id=" + id + ", name=" + name + ", email=" + email + ", birthDate=" + birthDate + ", baseSalary="
+				+ baseSalary + ", departmant=" + departmant + "]";
+	}
+}
